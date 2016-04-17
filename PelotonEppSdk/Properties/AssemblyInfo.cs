3f576dbab049ddd0aab55b3c2dd855b3e93e06db
Craig Attiwill <cattiwill@peloton-technologies.com>
2016-04-17 15:57:39 -0700

    Added company information to assembly Fixed settings / appconfig file which was corrupted Removed debug information from release setting

--- PelotonEppSdk/Properties/AssemblyInfo.cs
+++ PelotonEppSdk/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PelotonEppSdk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Peloton .NET SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Peloton Technologies Inc.")]
 [assembly: AssemblyProduct("PelotonEppSdk")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
