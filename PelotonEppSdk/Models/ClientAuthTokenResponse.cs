672ba8565bf2bd52aa3aab23c409932691b7cb03
Fraser DeLisle <fdelisle@peloton-technologies.com>
2017-03-02 19:20:26 -0800

    removed an errant line.

--- PelotonEppSdk/Models/ClientAuthTokenResponse.cs
+++ PelotonEppSdk/Models/ClientAuthTokenResponse.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using PelotonEppSdk.Enums;
 
-
 namespace PelotonEppSdk.Models
 {
     public class ClientAuthTokenResponse: Response
