91e699ae208d0e75a87a752d5363807c5474cf29
Fraser DeLisle <fdelisle@peloton-technologies.com>
2017-04-01 03:53:32 -0700

    implemented ValidationSubsetAttribute. Unit tests for validation subset attribute. New unit tests are using nUnit testing framework (may be temporary)

---
+++ PelotonEppSdk/Validations/ValidationSubsetAttribute.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace PelotonEppSdk.Validations
+{
+    public interface IValidationSubsetAttribute
+    {
+        Enum[] ValidationSubsetEnum { get; set; }
+    }
+
+    public class ValidationSubsetAttribute : Attribute, IValidationSubsetAttribute
+    {
+        public enum GeneralEnum
+        {
+            One,
+            Two,
+            Three
+        }
+
+        public Enum[] ValidationSubsetEnum { get; set; }
+        public Type EnumType;
+
+        public ValidationSubsetAttribute(Enum[] method)
+        {
+            ValidationSubsetEnum = method;
+            EnumType = method.GetType();
+        }
+
+        public ValidationSubsetAttribute(Enum method)
+        {
+            ValidationSubsetEnum = new [] { method };
+            EnumType = method.GetType();
+        }
+
+        public ValidationSubsetAttribute(GeneralEnum[] method)
+        {
+            ValidationSubsetEnum = ConvertToEnumArray(method);
+            EnumType = method.GetType();
+        }
+
+        public ValidationSubsetAttribute(GeneralEnum method)
+        {
+            ValidationSubsetEnum = ConvertToEnumArray(method);
+            EnumType = method.GetType();
+        }
+
+        protected ValidationSubsetAttribute()
+        {
+        }
+
+        public Enum[] ConvertToEnumArray<T>(T partitionEnum)
+        {
+            return new[]{  partitionEnum as Enum };
+        }
+
+        public Enum[] ConvertToEnumArray<T>(T[] partitionEnum)
+        {
+            return partitionEnum.Select(m => m as Enum).ToArray();
+        }
+    }
+
+    public class RequestMethodPartitionAttribute: ValidationSubsetAttribute
+    {
+        public enum RequestMethodEnum
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+        public RequestMethodPartitionAttribute(RequestMethodEnum[] method)
+        {
+            ValidationSubsetEnum = ConvertToEnumArray(method);
+        }
+
+        public RequestMethodPartitionAttribute(RequestMethodEnum method)
+        {
+            ValidationSubsetEnum = ConvertToEnumArray(method);
+        }
+    }
+
+}
