81a5b8618a1d7b589c85589b3ac4d813fa093e7b
Fraser DeLisle <fdelisle@peloton-technologies.com>
2019-01-14 16:06:10 -0800

    Revert copyright year to 2016

--- PelotonEppSdkTests/Properties/AssemblyInfo.cs
+++ PelotonEppSdkTests/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PelotonEppSdkTests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
